a676d96e7f71bab87eac7a170ec0010784ce36b1
Felipe Silva <105028185+felipeOliveira-1@users.noreply.github.com>
2023-07-06 22:32:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ keywords = ['Deus', 'Jesus', 'Bíblia', 'cristão', 'cristianismo', 'fé', 'igre
 if st.button('Gerar'):
     response = openai.ChatCompletion.create(
         model="gpt-4",
-        max_tokens=7000,
+        max_tokens=3500,
         messages=[
             {"role": "system", "content": system_message},
         ]
